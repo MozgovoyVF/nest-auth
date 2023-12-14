ee99d974258fe27688378c14487de5bb262a830c
Vladislav Mozgovoy <mozgovoy2796@gmail.com>
2023-12-14 16:15:58 +0300

    add user and crud api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post, UsePipes, ValidationPipe} from "@nestjs/common";
+import {UserService} from "./user.service";
+import {CreateUserDto} from "./dto/create-user.dto";
 
-@Controller('user')
-export class UserController {}
+@Controller("user")
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  createUser(@Body() dto: CreateUserDto) {
+    return this.userService.save(dto);
+  }
+
+  @Get(":idOrEmail")
+  findOneUser(@Param("idOrEmail") idOrEmail: string) {
+    return this.userService.findOne(idOrEmail);
+  }
+
+  @Delete(":id")
+  deleteUser(@Param("id", ParseUUIDPipe) id: string) {
+    return this.userService.delete(id);
+  }
+}
