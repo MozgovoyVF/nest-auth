ea124ca312b753900e72052a22d333d5be914ef8
Vladislav Mozgovoy <mozgovoy2796@gmail.com>
2023-12-14 21:14:35 +0300

    add roles guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,12 +5,15 @@ import {
   Get,
   Param,
   ParseUUIDPipe,
+  UseGuards,
   UseInterceptors,
 } from "@nestjs/common";
 import {UserService} from "./user.service";
 import {UserResponse} from "./responses";
-import {CurrentUser} from "@common/decorators";
+import {CurrentUser, Roles} from "@common/decorators";
 import {JwtPayload} from "@auth/interfaces";
+import {RolesGuard} from "@auth/guards/role.guard";
+import {Role} from "@prisma/client";
 
 @Controller("user")
 export class UserController {
@@ -34,5 +37,12 @@ export class UserController {
   @Delete(":id")
   deleteUser(@Param("id", ParseUUIDPipe) id: string, @CurrentUser() user: JwtPayload) {
     return this.userService.delete(id, user);
+  }
+
+  @UseGuards(RolesGuard)
+  @Roles(Role.ADMIN)
+  @Get()
+  me(@CurrentUser() user: JwtPayload) {
+    return user;
   }
 }
